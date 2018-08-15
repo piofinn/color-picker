935b3e50e55901f0f9e8f0d84a519f5b7bf93f68
Pio Rasch-Halvorsen <pio@swsg.no>
2018-08-15 16:19:04 +0200

    Added more tests

--- test/Swatch.spec.js
+++ test/Swatch.spec.js
@@ -1,34 +1,43 @@
-import { shallowMount, render } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import Swatch from "../src/Swatch.vue";
+import { hsvToRgb } from "../src/util/colorConversion.js";
 const expect = require('chai').expect;
 
-describe("Mocha", () => {
-  it("can assert", () => {
-    expect(1).to.equal(1);
-  });
-});
+
+// SET UP TEST INSTANCE WITH MOCK PROP
 
 const colorMock = {
-  id: 1,
+  id: 5,
   color: { h: 0, s: 0, v: 0 }
 }
+const wrapper = mount(Swatch, {
+  propsData: {
+    color: colorMock
+  }
+});
+
+// UNIT TESTS
 
 describe("Swatch.vue", () => {
-  it("mounts", () => {
-    const wrapper = shallowMount(Swatch, {
-      propsData: {
-        color: colorMock
-      }
-    });
-    expect(wrapper.contains('div.swatch')).to.be.true;
+  it("mounts as a div with class swatch", () => {
+    expect(wrapper.is('div.swatch')).to.be.true;
   });
 
-  it("renders an svg", () => {
-    const wrapper = shallowMount(Swatch, {
-      propsData: {
-        color: colorMock
-      }
-    });
-    expect(wrapper.contains('svg')).to.be.true;
-  })
+  it("correctly converts color value to hex", () => {
+    const controlHex = hsvToRgb(colorMock.color, true);
+    expect(wrapper.vm.hexValue).to.equal(controlHex);
+  });
+
+  it("renders a delete button as an svg", () => {
+    expect(wrapper.contains('svg.fa-trash')).to.be.true;
+  });
+
+  it("emits a delete-action when delete button is clicked", () => {
+    wrapper.find('.fa-trash').trigger('click');
+    expect(wrapper.emitted('delete-action')).to.have.lengthOf.least(1);
+  });
+
+  it("emits the correct id as payload on delete-action", () => {
+    expect(wrapper.emitted('delete-action')[0]).to.contain(5);
+  });
 })
